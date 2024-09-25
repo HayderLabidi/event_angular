f033acfe00ba9f3fe94b5338450a695e3a46f8bf
Hayder Labidi <108490838+HayderLabidi@users.noreply.github.com>
2024-09-25 19:55:18 +0100

    ok

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'event_angular'`, () => {
+  it(`should have as title 'event_project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('event_angular');
+    expect(app.title).toEqual('event_project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('event_angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('event_project app is running!');
   });
 });
